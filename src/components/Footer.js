6777f2378faf7ce469ab2d042bf92d7ac1cf672e
Ahmed Tawfiq <ahmed123t@gmail.com>
2022-06-02 15:24:24 +0300

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer>
+      <span>Designed by: Ahmed Tawfiq</span>
+      {"  "}
+      <span>2022©</span>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import { SearchContextProvider } from "./contexts/SearchContext";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
+import Footer from "./components/Footer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <SearchContextProvider>
     <Navbar />
     <App />
+    <Footer />
   </SearchContextProvider>
 );
